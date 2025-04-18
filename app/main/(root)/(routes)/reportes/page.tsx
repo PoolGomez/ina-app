3961673fe7af8e00c4bb31a80f7e0ae62caf2d48
unknown <pgomez4790@gmail.com>
2025-04-18 02:23:14 -0500

    reports maintence

--- app/main/(root)/(routes)/reportes/page.tsx
+++ app/main/(root)/(routes)/reportes/page.tsx
@@ -1,9 +1,18 @@
+import { Heading } from "../../_components/heading"
 
 
 const ReportesPage = () => {
   return (
-    <div>
-      Reportes Page
+    <div className="flex-col">
+      <div className="flex-1 space-y-5 p-4 pt-4">
+
+        <div className="flex items-center justify-between">
+            <Heading title="Reportes" description="En construcción" />
+
+        
+        </div>    
+
+      </div>
     </div>
   )
 }
